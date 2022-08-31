89093da9ee277b1fea7db1152a396050c6473ff3
worldofprem <psprem4459@gmail.com>
2022-08-31 18:52:49 +0530

    this is my first push in git

---
+++ main.py
@@ -0,0 +1,16 @@
+
+import  pymongo
+
+client = pymongo.MongoClient("mongodb+srv://psprem4459:eZNqHEV_8!s454h@cluster0.qofyebn.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
+
+d = {
+    "name": "prem",
+    "email": "psprem4459@gamil.com",
+    "surname": "sharma"
+}
+db1 = client['mongotest']
+coll = db1['test']
+coll.insert_one(d)
+
